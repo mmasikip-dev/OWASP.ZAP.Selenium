79e66dc3fa10d0d4f21761662f4c0a0bffdfda9a
MyNameIsMark <mmasikip13@gmail.com>
2020-11-16 18:05:34 +1300

    Initial project

---
+++ OWASP.ZAP.Selenium/OWASP.ZAP.Selenium/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OWASP.ZAP.Selenium")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("OWASP.ZAP.Selenium")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a53aac3-9c71-4374-9ab5-10dde4108ad6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
